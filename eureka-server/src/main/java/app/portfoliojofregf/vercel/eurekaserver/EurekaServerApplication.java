15cd2ac90d762b27ac74fe8fde3cf6836dc63198
Guillermo Jofre <jofregf@gmail.com>
2023-12-28 14:35:17 -0300

    feat: creacion de proyecto

---
+++ eureka-server/src/main/java/app/portfoliojofregf/vercel/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package app.portfoliojofregf.vercel.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
